424fb71d59c857f5b13f04af147b0f1afd653bf6
Michael Greene <a.minor.threat@gmail.com>
2014-04-23 23:51:13 -0700

    simplified bytes_to_int and int_to_bytes; added the ability for int_to_bytes to zero pad

--- pgpy/util.py
+++ pgpy/util.py
@@ -2,20 +2,21 @@
 
 utility functions for debutils.pgp
 """
+import binascii
+import math
+from enum import IntEnum
+
 
 def bytes_to_int(b):
-    n = 0
-    for octet in bytearray(b):
-        n += octet
-        n <<= 8
-    n >>= 8
+    return int(binascii.hexlify(b), 16)
 
-    return n
 
-def int_to_bytes(i):
-    b = []
-    while i > 0:
-        b.insert(0, i & 0xFF)
-        i >>= 8
+def int_to_bytes(i, minlen=1):
+    plen = max(int(math.ceil(i.bit_length() / 8.0)) * 2, (minlen*2))
+    hexstr = '{0:0{1}x}'.format(i, plen).encode()
+    return binascii.unhexlify(hexstr)
 
-    return bytes(b) if len(bytes(b)) == len(b) else ''.join([ chr(c) for c in b ])
+
+class PFIntEnum(IntEnum):
+    def __bytes__(self):
+        return int_to_bytes(self.value)
