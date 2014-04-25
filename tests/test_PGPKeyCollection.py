fb3db2ed4da2cd96daa2804824c18da31ef3c58c
Michael Greene <a.minor.threat@gmail.com>
2014-04-24 18:05:49 -0700

    added some skeleton to test_PGPKeyCollection, for later

--- tests/test_PGPKeyCollection.py
+++ tests/test_PGPKeyCollection.py
@@ -1,1 +1,20 @@
 import pytest
+from pgpy.key import PGPKeyCollection
+
+keys = [
+    "tests/testdata/debutils.key",
+    "tests/testdata/debutils.gpg",
+    "tests/testdata/debutils.sec.gpg"
+]
+keyids = [
+    "ascii",
+    "gpg-public",
+    "gpg-private"
+]
+@pytest.fixture(params=keys, ids=keyids)
+def load_key(request):
+    return PGPKeyCollection(request.param)
+
+
+class TestPGPKeyLoader:
+    pass
