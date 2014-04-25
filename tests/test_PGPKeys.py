f0d15612d5c98c8420239909dcfadbaffeb48d73
Michael Greene <a.minor.threat@gmail.com>
2014-04-25 04:56:59 -0700

    commented out the keyrings; these may need to move later.

--- tests/test_PGPKeys.py
+++ tests/test_PGPKeys.py
@@ -3,27 +3,30 @@ from pgpy.key import PGPPublicKey, PGPPrivateKey
 
 pubkeys = [
     "tests/testdata/debutils.key",
-    "tests/testdata/debutils.gpg",
+    # "tests/testdata/debutils.gpg",
     "tests/testdata/ftpmaster_ubuntu_2004.key",
     "tests/testdata/ftpmaster_ubuntu_2012.key",
     "tests/testdata/cdimage_ubuntu_2004.key",
     "tests/testdata/cdimage_ubuntu_2012.key",
-]
-privkeys = [
-    "tests/testdata/debutils.key",
-    "tests/testdata/debutils.sec.gpg",
+    # "tests/testdata/ubuntu_apt_trusted.gpg"
 ]
 keyids_pub = [
     "debutils-ascii",
-    "debutils-keyring",
+    # "debutils-keyring",
     "ubuntu-2004",
     "ubuntu-2012",
     "cdimage-2004",
     "cdimage-2012",
+    # "apt-keyring",
+]
+##TODO: need more private key test material
+privkeys = [
+    "tests/testdata/debutils.key",
+    # "tests/testdata/debutils.sec.gpg",
 ]
 keyids_priv = [
     "ascii",
-    "keyring"
+    # "keyring"
 ]
 @pytest.fixture(params=pubkeys, ids=keyids_pub)
 def load_pub(request):
@@ -36,10 +39,19 @@ def load_priv(request):
 
 class TestPGPPublicKey:
     def test_parse(self, load_pub):
-        # k = PGPPublicKey(load_pub)
-        pass
+        k = PGPPublicKey(load_pub)
+
+    def test_bytes(self, load_pub):
+        k = PGPPublicKey(load_pub)
+        assert k.__bytes__() == k.data
+
 
 class TestPGPPrivateKey:
     def test_parse(self, load_priv):
-        # k = PGPPrivateKey(load_priv)
+        k = PGPPrivateKey(load_priv)
+        pass
+
+    def test_bytes(self, load_priv):
+        k = PGPPrivateKey(load_priv)
+        assert k.__bytes__() == k.data
         pass
