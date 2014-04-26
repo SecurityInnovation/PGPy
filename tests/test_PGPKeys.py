f08c502e086875cd9e73867422e18bd1228407ea
Michael Greene <a.minor.threat@gmail.com>
2014-04-26 05:23:51 -0700

    added new test data; implemented new pgpdump test fixture

--- tests/test_PGPKeys.py
+++ tests/test_PGPKeys.py
@@ -1,11 +1,13 @@
 import pytest
 from pgpy.key import PGPPublicKey, PGPPrivateKey
+from pgpy.packet.pgpdump import PGPDumpFormat
 
 pubkeys = [
     "tests/testdata/TestRSA.key",
     "tests/testdata/TestDSAandElGamal.key",
     "tests/testdata/TestDSA.key",
     "tests/testdata/TestRSASignOnly.key",
+    "tests/testdata/TestProtected.key",
     "tests/testdata/ftpmaster_ubuntu_2004.key",
     "tests/testdata/ftpmaster_ubuntu_2012.key",
     "tests/testdata/cdimage_ubuntu_2004.key",
@@ -16,6 +18,7 @@ keyids_pub = [
     "test-dsa-elgamal",
     "test-dsa",
     "test-rsa-signonly",
+    "test-protected",
     "ubuntu-2004",
     "ubuntu-2012",
     "cdimage-2004",
@@ -23,16 +26,18 @@ keyids_pub = [
 ]
 ##TODO: need more private key test material
 privkeys = [
-    "tests/testdata/TestRSA.key",
-    "tests/testdata/TestDSAandElGamal.key",
-    "tests/testdata/TestDSA.key",
-    "tests/testdata/TestRSASignOnly.key",
+    "tests/testdata/TestRSA.sec.key",
+    "tests/testdata/TestDSAandElGamal.sec.key",
+    "tests/testdata/TestDSA.sec.key",
+    "tests/testdata/TestRSASignOnly.sec.key",
+    "tests/testdata/TestProtected.sec.key",
 ]
 keyids_priv = [
     "test-rsa",
     "test-dsa-elgamal",
     "test-dsa",
     "test-rsa-signonly",
+    "test-protected",
 ]
 @pytest.fixture(params=pubkeys, ids=keyids_pub)
 def load_pub(request):
@@ -44,8 +49,9 @@ def load_priv(request):
 
 
 class TestPGPPublicKey:
-    def test_parse(self, load_pub):
+    def test_parse(self, load_pub, pgpdump):
         k = PGPPublicKey(load_pub)
+        assert '\n'.join(PGPDumpFormat(k).out) + '\n' == pgpdump.decode()
 
     def test_bytes(self, load_pub):
         k = PGPPublicKey(load_pub)
@@ -55,9 +61,7 @@ class TestPGPPublicKey:
 class TestPGPPrivateKey:
     def test_parse(self, load_priv):
         k = PGPPrivateKey(load_priv)
-        pass
 
     def test_bytes(self, load_priv):
         k = PGPPrivateKey(load_priv)
         assert k.__bytes__() == k.data
-        pass
