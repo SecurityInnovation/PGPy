7ea080ae29b3d595b59f2ba1eeabf8222f2b0e79
Michael Greene <a.minor.threat@gmail.com>
2014-04-28 13:19:07 -0700

    added new key material to tests

--- tests/test_PGPKeys.py
+++ tests/test_PGPKeys.py
@@ -3,41 +3,53 @@ from pgpy.key import PGPPublicKey, PGPPrivateKey
 from pgpy.packet.pgpdump import PGPDumpFormat
 
 pubkeys = [
-    "tests/testdata/TestRSA.key",
-    "tests/testdata/TestDSAandElGamal.key",
-    "tests/testdata/TestDSA.key",
-    "tests/testdata/TestRSASignOnly.key",
-    "tests/testdata/TestProtected.key",
-    "tests/testdata/ftpmaster_ubuntu_2004.key",
-    "tests/testdata/ftpmaster_ubuntu_2012.key",
-    "tests/testdata/cdimage_ubuntu_2004.key",
-    "tests/testdata/cdimage_ubuntu_2012.key",
+    "tests/testdata/pubkeys/TestRSA.key",
+    "tests/testdata/pubkeys/TestDSAandElGamal.key",
+    "tests/testdata/pubkeys/TestDSA.key",
+    "tests/testdata/pubkeys/TestRSASignOnly.key",
+    "tests/testdata/pubkeys/ftpmaster_ubuntu_2004.key",
+    "tests/testdata/pubkeys/ftpmaster_ubuntu_2012.key",
+    "tests/testdata/pubkeys/cdimage_ubuntu_2004.key",
+    "tests/testdata/pubkeys/cdimage_ubuntu_2012.key",
+    "tests/testdata/pubkeys/TestKeyDecryption-DSA.key",
+    "tests/testdata/pubkeys/TestKeyDecryption-DSASignOnly.key",
+    "tests/testdata/pubkeys/TestKeyDecryption-RSA.key",
+    "tests/testdata/pubkeys/TestKeyDecryption-RSASignOnly.key",
 ]
 keyids_pub = [
     "test-rsa",
-    "test-dsa-elgamal",
     "test-dsa",
+    "test-dsa-signony",
     "test-rsa-signonly",
-    "test-protected",
     "ubuntu-2004",
     "ubuntu-2012",
     "cdimage-2004",
     "cdimage-2012",
+    "protected-dsa",
+    "protected-dsa-signonly",
+    "protected-rsa",
+    "protected-rsa-signonly",
 ]
 ##TODO: need more private key test material
 privkeys = [
-    "tests/testdata/TestRSA.sec.key",
-    "tests/testdata/TestDSAandElGamal.sec.key",
-    "tests/testdata/TestDSA.sec.key",
-    "tests/testdata/TestRSASignOnly.sec.key",
-    "tests/testdata/TestProtected.sec.key",
+    "tests/testdata/seckeys/TestRSA.sec.key",
+    "tests/testdata/seckeys/TestDSAandElGamal.sec.key",
+    "tests/testdata/seckeys/TestDSA.sec.key",
+    "tests/testdata/seckeys/TestRSASignOnly.sec.key",
+    "tests/testdata/seckeys/TestKeyDecryption-DSA.sec.key",
+    "tests/testdata/seckeys/TestKeyDecryption-DSASignOnly.sec.key",
+    "tests/testdata/seckeys/TestKeyDecryption-RSA.sec.key",
+    "tests/testdata/seckeys/TestKeyDecryption-RSASignOnly.sec.key",
 ]
 keyids_priv = [
     "test-rsa",
     "test-dsa-elgamal",
     "test-dsa",
     "test-rsa-signonly",
-    "test-protected",
+    "protected-dsa",
+    "protected-dsa-signonly",
+    "protected-rsa",
+    "protected-rsa-signonly",
 ]
 @pytest.fixture(params=pubkeys, ids=keyids_pub)
 def load_pub(request):
