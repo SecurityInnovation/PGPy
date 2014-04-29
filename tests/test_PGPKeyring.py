2bb1555058ee637cd8dd438a68ae328a83dc5119
Michael Greene <a.minor.threat@gmail.com>
2014-04-29 14:31:39 -0700

    added unit test for @managed (tests that decorated functions do not raise a PGPError when in a managed context, but do when outside of that context)

--- tests/test_PGPKeyring.py
+++ tests/test_PGPKeyring.py
@@ -1,7 +1,9 @@
 import pytest
+import os
 
 import pgpy
 from pgpy.pgpdump import PGPDumpFormat
+from pgpy.errors import PGPError
 
 keys = [
     "tests/testdata/testkeys.gpg",
@@ -17,24 +19,49 @@ keyids = [
 def load_key(request):
     return request.param
 
+akeys = [
+    [ k for k in os.listdir("tests/testdata/pubkeys/") if k[-4:] == ".key" ],
+    [ k for k in os.listdir("tests/testdata/seckeys/") if k[-8:] == ".sec.key" ],
+]
+akeys.append(keys[0] + keys[1])
+akeyids = [
+    "pubkeys",
+    "seckeys",
+    "both"
+]
+
+@pytest.fixture(params=akeys, ids=akeyids)
+def load_akey(request):
+    return request.param
+
 
 class TestPGPKeyring:
     def test_load(self, load_key, pgpdump):
-        with pgpy.PGPKeyring(load_key) as k:
-            assert '\n'.join(PGPDumpFormat(k).out) + '\n' == pgpdump.decode()
+        k = pgpy.PGPKeyring(load_key)
+
+        assert '\n'.join(PGPDumpFormat(k).out) + '\n' == pgpdump.decode()
+
+    ##TODO: test keyring contents against ascii armored key contents
+    # def test_load2(self, load_key, load_akey):
+    #     pass
+
 
     def test_bytes(self, load_key):
-        with pgpy.PGPKeyring(load_key) as k:
-            fb = b''.join([open(f, 'rb').read() for f in load_key]) if type(load_key) is list else open(load_key, 'rb').read()
+        k = pgpy.PGPKeyring(load_key)
+        fb = b''.join([open(f, 'rb').read() for f in load_key]) if type(load_key) is list else open(load_key, 'rb').read()
 
-            assert k.__bytes__() == fb
+        assert k.__bytes__() == fb
 
-    # def test_list_pubkeys(self, load_key):
+    ##TODO: test bytes
+    # def test_str(self, load_key):
     #     pass
 
-    # def test_list_privkeys(self, load_key):
-    #     pass
+    def test_managed(self):
+        k = pgpy.PGPKeyring()
+        with k.key():
+            k.sign(None)
+            k.verify(None)
 
-    def test_str(self, load_key):
-        with pgpy.PGPKeyring(load_key) as k:
-            pass
+        with pytest.raises(PGPError):
+            k.sign(None)
+            k.verify(None)
