7be8dd5e89784bfe7a1b6c9a102f9fbfa57de80d
Michael Greene <a.minor.threat@gmail.com>
2014-04-28 21:02:58 -0700

    added test_keyid and test_fingerprint

--- tests/test_PGPKeys.py
+++ tests/test_PGPKeys.py
@@ -1,7 +1,10 @@
 import pytest
+import subprocess
+import re
 
 from pgpy.pgp import PGPLoad, PGPKey
 from pgpy.pgpdump import PGPDumpFormat
+from pgpy.packet.fields import Header
 
 pubkeys = [
     "tests/testdata/pubkeys/TestRSA.key",
@@ -42,14 +45,14 @@ privkeys = [
     "tests/testdata/seckeys/TestKeyDecryption-RSASignOnly.sec.key",
 ]
 keyids_priv = [
-    "test-rsa",
-    "test-dsa-elgamal",
-    "test-dsa",
-    "test-rsa-signonly",
-    "protected-dsa",
-    "protected-dsa-signonly",
-    "protected-rsa",
-    "protected-rsa-signonly",
+    "sec-test-rsa",
+    "sec-test-dsa-elgamal",
+    "sec-test-dsa",
+    "sec-test-rsa-signonly",
+    "sec-protected-dsa",
+    "sec-protected-dsa-signonly",
+    "sec-protected-rsa",
+    "sec-protected-rsa-signonly",
 ]
 @pytest.fixture(params=pubkeys + privkeys, ids=keyids_pub + keyids_priv)
 def load_key(request):
@@ -69,10 +72,31 @@ class TestPGPKey:
         k = PGPLoad(load_key)[0]
         k.crc == k.crc24()
 
+    def test_keyid(self, load_key):
+        k = PGPLoad(load_key)[0]
+        spkt = [pkt.unhashed_subpackets for pkt in k.packets if pkt.header.tag == Header.Tag.Signature][0]
+
+        assert k.keyid == spkt.Issuer.payload.decode()
+
+    def test_fingerprint(self, load_key):
+        k = PGPLoad(load_key)[0]
+        kfp = [ k.fingerprint[i:(i+4)] for i in range(0, len(k.fingerprint), 4)]
+        kfp[4] += ' '
+        kfp = ' '.join(kfp)
+        fp = subprocess.check_output(['gpg',
+                                      '--no-default-keyring',
+                                      '--keyring', 'tests/testdata/testkeys.gpg',
+                                      '--secret-keyring', 'tests/testdata/testkeys.sec.gpg',
+                                      '--fingerprint', k.keyid])
+
+        assert kfp == re.search(r'Key fingerprint = ([0-9A-F ]*)', fp.decode()).group(1)
+
     def test_str_pub(self, load_key):
         k = PGPLoad(load_key)[0]
+
         assert str(k) == k.bytes.decode()
 
     def test_bytes(self, load_key):
         k = PGPLoad(load_key)[0]
+
         assert k.__bytes__() == k.data
