31e5ec5b86448208e3cf00ac2ec6ded23a0b902d
Michael Greene <a.minor.threat@gmail.com>
2014-04-30 17:05:56 -0700

    moved PFIntEnum to packets.type

--- pgpy/util.py
+++ pgpy/util.py
@@ -4,7 +4,6 @@ utility functions for debutils.pgp
 """
 import binascii
 import math
-from enum import IntEnum
 
 
 def bytes_to_int(b):
@@ -15,8 +14,3 @@ def int_to_bytes(i, minlen=1):
     plen = max(int(math.ceil(i.bit_length() / 8.0)) * 2, (minlen * 2))
     hexstr = '{0:0{1}x}'.format(i, plen).encode()
     return binascii.unhexlify(hexstr)
-
-
-class PFIntEnum(IntEnum):
-    def __bytes__(self):
-        return int_to_bytes(self.value)
