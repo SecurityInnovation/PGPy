a0542db9fedfdb36a66969062704ed48c7ae3eca
Michael Greene <a.minor.threat@gmail.com>
2014-05-01 18:10:41 -0700

    changed from StrictVersion to LooseVersion

--- pgpy/_author.py
+++ pgpy/_author.py
@@ -5,9 +5,9 @@ __version__ is a PEP-386 compliant version string,
 making use of distutils.version.StrictVersion
 """
 
-from distutils.version import StrictVersion
+from distutils.version import LooseVersion
 
 __author__ = "Michael Greene"
 __copyright__ = "Copyright (c) 2014 Michael Greene"
 __license__ = "MIT"
-__version__ = str(StrictVersion("0.0.0"))
+__version__ = str(LooseVersion("0.0.0"))
