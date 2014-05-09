0d79bc2dee94bfaf4ca6ee41c58d53cfa6cb824f
Michael Greene <a.minor.threat@gmail.com>
2014-05-09 15:58:22 -0700

    refactored test fixtures - closes #52 and #51

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,103 +1,33 @@
-import pytest
 import os
-import subprocess
+import sys
+
 from distutils.version import LooseVersion
 
 from cryptography.hazmat.backends import openssl
 
 openssl_ver = LooseVersion(openssl.backend.openssl_version_text().split(' ')[1])
 
-def pytest_runtest_setup(item):
-    print("Using OpenSSL " + str(openssl_ver) + "\n")
+# set the CWD to the project root if it isn't already
+if 'PGPy' in os.getcwd():
+    while os.path.basename(os.getcwd()) != 'PGPy':
+        os.chdir('..')
 
-##TODO: refactor this a bit so that it gives more finely grained options and also nicely sorted output
-class TestFiles(object):
-    @staticmethod
-    def id(file):
-        f = file.split('/')[-1]
+else:
+    raise Exception("Could not set the proper expected working directory!")
 
-        if f[:4] == "Test":
-            if f[-8:] == '.sec.key':
-                return 'sec-{kalg}{encalg}-{bitlen}'.format(
-                    kalg=f.split('-')[0][4:].lower(),
-                    encalg='' if 'Enc' not in f else '-' + f.split('-')[1][3:].lower(),
-                    bitlen=f.split('-')[-1][:4]
-                )
+# now import stuff from fixtures so it can be imported by test modules
+from fixtures import TestFiles, pgpdump, gpg_verify
 
-            else:
-                return '{kalg}{encalg}-{bitlen}'.format(
-                    kalg=f.split('-')[0][4:].lower(),
-                    encalg='' if 'Enc' not in f else '-' + f.split('-')[1][3:].lower(),
-                    bitlen=f.split('-')[-1][:4]
-                )
+# pytest hooks
+# called after command line options have been parsed and all plugins and initial conftest files been loaded.
+def pytest_configure(config):
+    assert os.path.basename(os.getcwd()) == 'PGPy'
 
-        if f[-4:] == '.key':
-            return file.replace('_', '-').split('.')[0].split('/')[-1]
-
-        if f.split('.')[0] == 'signed_message':
-            return f.split('.')[1].lower()
-
-        if f[:7] == 'Release':
-            return file.split('/')[-2]
-
-    @property
-    def pubids(self):
-        return [ TestFiles.id(key) for key in self.pubs ]
-
-    @property
-    def privids(self):
-        return [ TestFiles.id(key) for key in self.privs ]
-
-    @property
-    def keys(self):
-        return self.pubs + self.privs
-
-    @property
-    def keyids(self):
-        return self.pubids + self.privids
-
-    @property
-    def enckeys(self):
-        return [ key for key in self.privs if 'Enc' in key ]
-
-    @property
-    def enckeyids(self):
-        return [ TestFiles.id(key) for key in self.enckeys ]
-
-    @property
-    def sigids(self):
-        return [ TestFiles.id(sig) for sig in self.sigs ]
-
-    def __init__(self):
-        self.pubs = [ 'tests/testdata/pubkeys/' + k for k in os.listdir('tests/testdata/pubkeys') if k[-4:] == '.key' ]
-        self.privs = [ 'tests/testdata/seckeys/' + k for k in os.listdir('tests/testdata/seckeys') if k[-4:] == '.key' ]
-
-        self.sigs = [ 'tests/testdata/signatures/' + k for k in os.listdir('tests/testdata/signatures') if k[-4:] == '.asc' ]
-        self.sigm = [ 'tests/testdata/signed_message', ] * len(self.sigs)
-        self.sigs += [ 'tests/testdata/' + d + '/Release.gpg' for d in ['aa-testing', 'debian-sid', 'ubuntu-precise'] ]
-        self.sigm += [ 'tests/testdata/' + d + '/Release' for d in ['aa-testing', 'debian-sid', 'ubuntu-precise']]
+    # display the working directory and the OpenSSL version
+    print("Working Directory: " + os.getcwd())
+    print("Using OpenSSL " + str(openssl_ver))
+    print()
 
 
-tf = TestFiles()
-
-##TODO: rewrite this fixture as a closure that returns a pgpdump function that takes input
-##TODO: write a similar fixture using a closure for doing things using GPG (such as signature validation)
-@pytest.fixture()
-def pgpdump(request):
-    pgpd_args = ['pgpdump', '-i', '-l', '-m', '-p', '-u', '--']
-
-    if 'test_PGPSignature.TestPGPSignature' in str(request.cls):
-        pgpd_args.append(request._funcargs['pgpsig'])
-
-    if 'load_key' in request._funcargs.keys():
-        if type(request._funcargs['load_key']) is str:
-            pgpd_args.append(request._funcargs['load_key'])
-
-        else:
-            o = b''
-            for f in request._funcargs['load_key']:
-                o += subprocess.check_output(pgpd_args + [f])
-
-            return o
-
-    return subprocess.check_output(pgpd_args)
+# and set __all__
+__all__ = [openssl_ver, TestFiles, pgpdump, gpg_verify]
---
+++ tests/fixtures.py
@@ -0,0 +1,170 @@
+import functools
+import os
+import subprocess
+
+import pytest
+
+# fixture utilities
+class CWD_As(object):
+    def __init__(self, newwd):
+        if not os.path.exists(newwd):
+            raise FileNotFoundError(newwd)
+
+        self.oldwd = os.getcwd()
+        self.newwd = newwd
+
+    def __call__(self, func):
+        @functools.wraps(func)
+        def setcwd(*args, **kwargs):
+            os.chdir(self.newwd)
+            fo = func(*args, **kwargs)
+            os.chdir(self.oldwd)
+            return fo
+        return setcwd
+
+
+class ClassProperty(property):
+    def __get__(self, cls, owner):
+        return self.fget.__get__(None, owner)()
+
+
+# test fixtures
+
+
+class TestFiles(object):
+    # this is a purely static class.
+    # there are two reasons I am grouping these fixture methods into a class:
+    # 1. to group them syntactically because they are similar and related
+    # 2. to group them syntactically with abstracted support functions that are used by the fixture methods
+
+    # keys under testdata/pubkeys/
+    _public___keys = [ k for k in os.listdir('tests/testdata/pubkeys')
+                              if k.split('.')[-1] == 'key']
+    _private__keys = [ k for k in os.listdir('tests/testdata/seckeys')
+                              if k.split('.')[-1] == '.sec.key']
+    _detached_sigs = sorted([ s for s in os.listdir('tests/testdata/signatures')
+                              if s.split('.')[0]  == 'signed_message'])
+    _inline___sigs = sorted([ s for s in os.listdir('tests/testdata/signatures')
+                              if s.split('.')[0]  == 'inline_signed_message'])
+    _deb__releases = sorted([ os.path.join(d, r)
+                                for d in ['aa-testing', 'debian-sid', 'ubuntu-precise']
+                                for r in os.listdir('tests/testdata/' + d)
+                              if r == 'Release' ])
+    _deb_rel__sigs = sorted([ r + '.gpg' for r in _deb__releases ])
+
+    @staticmethod
+    def test_id(file):
+        # generate a test_id from a filename
+        f = file.split('/')[-1]
+
+        if f[:4] == "Test":
+            if f[-8:] == '.sec.key':
+                return 'sec-{kalg}{encalg}-{bitlen}'.format(
+                    kalg=f.split('-')[0][4:].lower(),
+                    encalg='' if 'Enc' not in f else '-' + f.split('-')[1][3:].lower(),
+                    bitlen=f.split('-')[-1][:4]
+                )
+
+            else:
+                return '{kalg}{encalg}-{bitlen}'.format(
+                    kalg=f.split('-')[0][4:].lower(),
+                    encalg='' if 'Enc' not in f else '-' + f.split('-')[1][3:].lower(),
+                    bitlen=f.split('-')[-1][:4]
+                )
+
+        if f[-4:] == '.key':
+            return file.replace('_', '-').split('.')[0].split('/')[-1]
+
+        if f.split('.')[0] == 'signed_message':
+            return f.split('.')[1].lower()
+
+        if f[:7] == 'Release':
+            return file.split('/')[-2]
+
+    @staticmethod
+    def ids(params):
+        return [ TestFiles.test_id(param) for param in params ]
+
+    @ClassProperty
+    @classmethod
+    def signatures(cls):
+        return [ 'signatures/' + s for s in cls._detached_sigs ] + cls._deb_rel__sigs
+
+    @ClassProperty
+    @classmethod
+    def sigsubjects(cls):
+        return [ 'signatures/' + s for s in (['signed_message',] * len(cls._detached_sigs)) ] + cls._deb__releases
+
+    @ClassProperty
+    @classmethod
+    def pubkeys(cls):
+        return [ 'pubkeys/' + pk
+                 for pk in sorted(cls._public___keys) ]
+
+    @ClassProperty
+    @classmethod
+    def privkeys(cls):
+        return sorted(cls._private__keys)
+
+    @ClassProperty
+    @classmethod
+    def unprotected_privkeys(cls):
+        return [ 'seckeys/' + pk
+                 for pk in sorted([ pk for pk in cls._private__keys if 'Enc' not in pk ]) ]
+
+    @ClassProperty
+    @classmethod
+    def protected_privkeys(cls):
+        return [ 'seckeys/' + pk
+                 for pk in sorted([ pk for pk in cls._private__keys if 'Enc' in pk ]) ]
+
+
+@pytest.fixture()
+def pgpdump():
+    @CWD_As('tests/testdata')
+    def _pgpdump(pgpdpath):
+        if not os.path.exists(pgpdpath):
+            raise FileNotFoundError(pgpdpath)
+
+        pgpd_args = ['/usr/bin/pgpdump', '-i', '-l', '-m', '-p', '-u', '--', pgpdpath]
+
+        try:
+            return subprocess.check_output(pgpd_args, stderr=subprocess.STDOUT)
+
+        ##TODO: add another exception clause here for catching when pgpdump does not exist
+        # pgpdump execution returned non-zero for some reason
+        except subprocess.CalledProcessError as e:
+            return "/usr/bin/pgpdump returned {ret}\n"\
+                   "===============================\n"\
+                   "{out}".format(ret=e.returncode, out=e.output)
+
+    return _pgpdump
+
+@pytest.fixture()
+def gpg_verify():
+    @CWD_As('tests/testdata')
+    def _gpg_verify(gpg_subjpath, gpg_sigpath=None):
+        gpg_args = ['/usr/bin/gpg',
+                '--no-default-keyring',
+                '--keyring', './testkeys.gpg',
+                '--secret-keyring', './testkeys.sec.gpg',
+                '--trustdb-name', './testkeys.trust',
+                '-vv', '--verify']
+
+        if gpg_sigpath is not None:
+            gpg_args += [gpg_sigpath]
+        gpg_args += gpg_subjpath
+
+        try:
+            return subprocess.check_output(gpg_args, stderr=subprocess.STDOUT)
+
+        except subprocess.CalledProcessError as e:
+            return "/usr/bin/gpg returned {ret}\n"\
+                   "===========================\n"\
+                   "{out}".format(ret=e.returncode, out=e.output)
+
+    return _gpg_verify
+
+
+# only allow a handful of things to be imported
+__all__ = [pgpdump, gpg_verify]
