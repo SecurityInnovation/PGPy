27e62bc73159e360081e0a0cb5bd8463011c708d
Michael Greene <a.minor.threat@gmail.com>
2014-05-09 20:36:31 -0700

    new exception - #50

--- pgpy/errors.py
+++ pgpy/errors.py
@@ -11,6 +11,10 @@ class PGPKeyDecryptionError(Exception):
     """Raised when decryption fails"""
     pass
 
+class PGPOpenSSLCipherNotSupported(Exception):
+    """Raised when OpenSSL does not support the requested cipher"""
+    pass
+
 
 class WontImplementError(NotImplementedError):
     """Raised when something that is not implemented, will not be implemented"""
