bf3acc5fb38476c18ac1cb7de3a1fb9126e957df
Michael Greene <a.minor.threat@gmail.com>
2014-06-02 13:22:28 -0700

    formatting

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # from distutils.core import setup
 from setuptools import setup
-from pip.req import parse_requirements
 
 # this is dirty
 import sys
@@ -12,13 +11,9 @@ import _author
 with open('README.rst') as readme:
     long_desc = readme.read()
 
-# requirements generators
-reqs = [ str(ir.req) for ir in parse_requirements('requirements.txt') ]
-test_reqs = parse_requirements('requirements-test.txt')
-
 ##TODO: fill in blank fields
 setup(
-    # basic metadata
+    # metadata
     name             = 'PGPy',
     version          = _author.__version__,
     description      = 'Pretty Good Privacy for Python',
@@ -26,35 +21,38 @@ setup(
     author           = _author.__author__,
     author_email     = "mgreene@securityinnovation.com",
     license          = _author.__license__,
+    classifiers      = ['Development Status :: 4 - Beta',
+                        'Operating System :: POSIX :: Linux',
+                        'Operating System :: MacOS :: MacOS X',
+                        'Operating System :: Microsoft :: Windows',
+                        'Intended Audience :: Developers',
+                        'Programming Language :: Python',
+                        'Programming Language :: Python :: 3.4',
+                        'Programming Language :: Python :: 3.3',
+                        'Programming Language :: Python :: 3.2',
+                        'Programming Language :: Python :: 2.7',
+                        'Programming Language :: Python :: Implementation :: CPython',
+                        'Topic :: Security',
+                        'Topic :: Security :: Cryptography',
+                        'Topic :: Software Development :: Libraries',
+                        'Topic :: Software Development :: Libraries :: Python Modules',
+                        'License :: OSI Approved :: BSD License'],
+    keywords        = ["PGP",
+                       "pgp",
+                       "Pretty Good Privacy",
+                       "GPG",
+                       "gpg",
+                       "OpenPGP"],
 
     # dependencies
-    install_requires = reqs,
+    install_requires = ['cryptography==0.4',
+                        'requests',
+                        'enum34'],
 
-    # # urls
+    # urls
     url              = "https://github.com/Commod0re/PGPy",
     download_url     = "https://github.com/Commod0re/PGPy/archive/{pgpy_ver}.tar.gz".format(pgpy_ver=_author.__license__),
     # bugtrack_url     = "https://github.com/Commod0re/PGPy/issues",
-
-    # classifiers, keywords, etc
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Security',
-        'Topic :: Security :: Cryptography',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: BSD License',
-    ],
-    keywords = ["PGP", "pgp", "Pretty Good Privacy", "GPG", "gpg", "OpenPGP"],
 
     # package hierarchy
     packages = [
