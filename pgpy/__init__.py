b94b2bb1dfb087a997098381a515fe287956d43a
Michael Greene <a.minor.threat@gmail.com>
2014-07-01 14:09:53 -0700

    simplified

--- pgpy/__init__.py
+++ pgpy/__init__.py
@@ -4,14 +4,6 @@ from ._author import __author__
 from ._author import __copyright__
 from ._author import __license__
 from ._author import __version__
-from .errors import PGPError
-from .errors import PGPKeyDecryptionError
-from .keys import PGPKeyring
 
-__all__ = [__author__,
-           __copyright__,
-           __license__,
-           __version__,
-           PGPError,
-           PGPKeyDecryptionError,
-           PGPKeyring]
+from .errors import *
+from .keys import PGPKeyring
