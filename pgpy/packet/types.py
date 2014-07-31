407bbf2c115bd53cf074c22fca4d5e7ff947d77a
Michael Greene <a.minor.threat@gmail.com>
2014-07-30 22:43:57 -0700

    this now uses the new object model for dispatching Packets, but so far only Opaque is implemented.

--- pgpy/packet/types.py
+++ pgpy/packet/types.py
@@ -1,186 +1,89 @@
 """ types.py
 """
-from enum import IntEnum
+import abc
 
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.ciphers import algorithms
+from .fields import Header
 
-from ..errors import PGPOpenSSLCipherNotSupported
-from ..util import int_to_bytes
+from ..decorators import TypedProperty
 
+from ..types import Dispatchable
 
-class PFIntEnum(IntEnum):
+class Packet(Dispatchable):
+    __slots__ = ['header']
+    __typeid__ = -1
+    __headercls__ = Header
+
+    def __init__(self):
+        super(Packet, self).__init__()
+        self.header = Header()
+
+    @abc.abstractmethod
     def __bytes__(self):
-        return int_to_bytes(self.value)
+        return self.header.__bytes__()
+
+    def __len__(self):
+        return len(self.header) + self.header.length
+
+    def __repr__(self):
+        return "<{} [tag 0x{:02x} at 0x{:x}>".format(self.__class__.__name__, self.header.tag, id(self))
+
+    @abc.abstractmethod
+    def parse(self, packet):
+        if self.header.tag == 0:
+            self.header.parse(packet)
 
 
-class PubKeyAlgo(PFIntEnum):
-    Invalid = 0x00
-    RSAEncryptOrSign = 0x01
-    RSAEncrypt = 0x02
-    RSASign = 0x03
-    ElGamal = 0x10
-    DSA = 0x11
+class VersionedPacket(Packet):
+    __slots__ = ['_version']
 
-    def __str__(self):
-        algos = {'RSAEncryptOrSign': "RSA Encrypt or Sign",
-                 'ElGamal': "ElGamal Encrypt-Only",
-                 'DSA': "DSA Digital Signature Algorithm"}
-
-        if self.name in algos.keys():
-            return algos[self.name]
-
-        raise NotImplementedError(self.name)  # pragma: no cover
+    @TypedProperty
+    def version(self):
+        return self._version
+    @version.int
+    def version(self, val):
+        self._version = val
+    @version.bytearray
+    @version.bytes
+    def version(self, val):
+        self.version = self.bytes_to_int(val)
 
 
-class SymmetricKeyAlgo(PFIntEnum):
-    Plaintext = 0x00
-    IDEA = 0x01
-    TripleDES = 0x02
-    CAST5 = 0x03
-    Blowfish = 0x04
-    AES128 = 0x07
-    AES192 = 0x08
-    AES256 = 0x09
-    Twofish256 = 0x0A
-    Camellia128 = 0x0B
-    Camellia192 = 0x0C
-    Camellia256 = 0x0D
+    def __init__(self):
+        super(VersionedPacket, self).__init__(self)
+        self.version = 4
 
-    @property
-    def block_size(self):
-        if self == SymmetricKeyAlgo.Plaintext:
-            return 0
-
-        if self in [SymmetricKeyAlgo.IDEA,
-                    SymmetricKeyAlgo.CAST5,
-                    SymmetricKeyAlgo.TripleDES,
-                    SymmetricKeyAlgo.Blowfish]:
-            return 64
-
-        if self in [SymmetricKeyAlgo.AES128,
-                    SymmetricKeyAlgo.AES192,
-                    SymmetricKeyAlgo.AES256,
-                    SymmetricKeyAlgo.Camellia128,
-                    SymmetricKeyAlgo.Camellia192,
-                    SymmetricKeyAlgo.Camellia256,
-                    SymmetricKeyAlgo.Twofish256]:
-            return 128
-
-        raise NotImplementedError(self.name)  # pragma: no cover
-
-    @property
-    def keylen(self):
-        if self in [SymmetricKeyAlgo.IDEA,
-                    SymmetricKeyAlgo.CAST5,
-                    SymmetricKeyAlgo.AES128,
-                    SymmetricKeyAlgo.Camellia128,
-                    SymmetricKeyAlgo.Blowfish]:
-            return 128
-
-        if self in [SymmetricKeyAlgo.AES192,
-                    SymmetricKeyAlgo.Camellia192,
-                    SymmetricKeyAlgo.TripleDES]:
-            return 192
-
-        if self in [SymmetricKeyAlgo.AES256,
-                    SymmetricKeyAlgo.Camellia256,
-                    SymmetricKeyAlgo.Twofish256]:
-            return 256
-
-        raise NotImplementedError(self.name)  # pragma: no cover
-
-    @property
-    def backend_alg_name(self):
-        return self.name if self.name[-3:] not in ['128', '192', '256'] else self.name[:-3]
-
-    @property
-    def decalg(self):
-        if hasattr(algorithms, self.backend_alg_name):
-            return getattr(algorithms, self.backend_alg_name)
-
-        else:
-            raise PGPOpenSSLCipherNotSupported("Algorithm " +
-                                               self.backend_alg_name +
-                                               " not supported by OpenSSL")
-
-    @property
-    def encalg(self):
-        # encryption cipher
-        pass
-
-    def __str__(self):
-        if self in [SymmetricKeyAlgo.CAST5,
-                    SymmetricKeyAlgo.Blowfish,
-                    SymmetricKeyAlgo.IDEA]:
-            return self.name
-
-        if self == SymmetricKeyAlgo.TripleDES:
-            return "Triple-DES"
-
-        if self in [SymmetricKeyAlgo.AES128,
-                    SymmetricKeyAlgo.AES192,
-                    SymmetricKeyAlgo.AES256,
-                    SymmetricKeyAlgo.Camellia128,
-                    SymmetricKeyAlgo.Camellia192,
-                    SymmetricKeyAlgo.Camellia256,
-                    SymmetricKeyAlgo.Twofish256]:
-            return "{alg} with {keysize}-bit key".format(alg=self.name[:-3], keysize=self.name[-3:])
-
-        raise NotImplementedError(self.name)  # pragma: no cover
+    @abc.abstractmethod
+    def parse(self, packet):
+        super(VersionedPacket, self).parse(packet)
+        self.version = packet[0]
+        del packet[0]
 
 
-class CompressionAlgo(PFIntEnum):
-    Uncompressed = 0x00
-    ZIP = 0x01
-    ZLIB = 0x02
-    BZ2 = 0x03
+class Opaque(Packet):
+    __slots__ = ['_payload']
+    __typeid__ = None
 
-    def __str__(self):
-        algos = {'Uncompressed': "Uncompressed",
-                 'ZIP': "ZIP <RFC1951>",
-                 'ZLIB': "ZLIB <RFC1950>",
-                 'BZ2': "BZip2"}
+    @TypedProperty
+    def payload(self):
+        return self._payload
+    @payload.bytearray
+    @payload.bytes
+    def payload(self, val):
+        self._payload = val
 
-        if self.name in algos.keys():
-            return algos[self.name]
+    def __init__(self):
+        super(Opaque, self).__init__()
+        self.payload = b''
 
-        raise NotImplementedError(self.name)  # pragma: no cover
+    def __bytes__(self):
+        _bytes = super(Opaque, self).__bytes__()
+        _bytes += self.payload
+        return _bytes
 
+    def __pgpdump__(self):
+        raise NotImplementedError()
 
-class HashAlgo(PFIntEnum):
-    Invalid = 0x00
-    MD5 = 0x01
-    SHA1 = 0x02
-    RIPEMD160 = 0x03
-    SHA256 = 0x08
-    SHA384 = 0x09
-    SHA512 = 0x0A
-    SHA224 = 0x0B
-
-    @property
-    def digestlen(self):
-        if self == HashAlgo.MD5:
-            return 128
-
-        if self in [HashAlgo.SHA1,
-                    HashAlgo.RIPEMD160]:
-            return 160
-
-        if self in [HashAlgo.SHA256,
-                    HashAlgo.SHA384,
-                    HashAlgo.SHA512,
-                    HashAlgo.SHA224]:
-            return int(self.name[-3:])
-
-        raise NotImplementedError(self.name)  # pragma: no cover
-
-    @property
-    def hasher(self):
-        if hasattr(hashes, self.name):
-            return getattr(hashes, self.name)()
-
-        raise NotImplementedError(self.name)  # pragma: no cover
-
-    def __str__(self):
-        return self.name
+    def parse(self, packet):
+        super(Opaque, self).parse(packet)
+        self.payload = packet[:self.header.length]
+        del packet[:self.header.length]
