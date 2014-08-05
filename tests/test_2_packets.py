e2614b188eff33bfa599d1f4324a8a727cec01a0
Michael Greene <a.minor.threat@gmail.com>
2014-08-05 13:13:03 -0700

    more new test material

--- tests/test_2_packets.py
+++ tests/test_2_packets.py
@@ -5,7 +5,7 @@ import pytest
 import os
 import re
 
-from pgpy.packet.fields import Header
+from pgpy.packet.types import Header
 from pgpy.packet import Packet
 from pgpy.packet import Opaque
 
@@ -26,16 +26,16 @@ _pclasses = {
     0x02: ['SignatureV4'],
     # 0x03: [''], ##TODO: name this
     # 0x04: [''], ##TODO: name this
-    # 0x05: ['PrivKeyV4'], ##TODO: uncomment when class is turned back on
-    # 0x06: ['PubKeyV4'], ##TODO: uncomment when class is turned back on
-    # 0x07: ['PrivSubKeyV4]', ##TODO: uncomment when class is turned back on
+    0x05: ['PrivKeyV4'],
+    0x06: ['PubKeyV4'],
+    0x07: ['PrivSubKeyV4'],
     # 0x08: ['CompressedData'], ##TODO: uncomment when class is turned back on
     # 0x09: [''], ##TODO: name this
     # 0x0A: [''], ##TODO: name this
     # 0x0B: ['LiteralData'], ##TODO: uncomment when class is written
     # 0x0C: ['Trust'], ##TODO: uncomment when class is turned back on
     # 0x0D: ['UserID'], ##TODO: uncomment when class is turned back on
-    # 0x0E: ['PubSubKey'], ##TODO: uncomment when class is turned back on
+    0x0E: ['PubSubKeyV4'],
     # 0x11: ['UserAttribute'], ##TODO: uncomment when class is turned back on
     # 0x12: [''], ##TODO: name this
     # 0x13: ['',] ##TODO: name this
