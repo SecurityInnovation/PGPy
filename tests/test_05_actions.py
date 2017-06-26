32af5367478b023cd2d81477157afb9d5931289e
J08nY <johny@neuromancer.sk>
2017-06-26 18:30:06 +0200

    Add tests for decrypting PKESKs with thrown keyids.

--- tests/test_05_actions.py
+++ tests/test_05_actions.py
@@ -865,6 +865,8 @@ class TestPGPKey_Actions(object):
         mtxt = "This message will have been encrypted"
         msg = PGPMessage.new(mtxt)
         emsg = pub.encrypt(msg, cipher=cipher, throw_keyid=True)
+        self.msgs[(pub.fingerprint, cipher, True)] = emsg
+
         assert len(emsg.encrypters) == 1
         assert emsg.encrypters.pop() == '0000000000000000'
 
@@ -878,6 +880,31 @@ class TestPGPKey_Actions(object):
             pytest.skip('Message not present; see test_encrypt_message skip or xfail reason')
 
         emsg = self.msgs[(sec.fingerprint, cipher)]
+        dmsg = sec.decrypt(emsg)
+
+        assert dmsg.message == "This message will have been encrypted"
+
+        # now check with GnuPG, if possible
+        if gpg_ver < '2.1' and sec.key_algorithm in {PubKeyAlgorithm.ECDSA, PubKeyAlgorithm.ECDH}:
+            # GnuPG prior to 2.1.x does not support EC* keys, so skip this step
+            return
+
+        with tempfile.NamedTemporaryFile('w+') as emsgf:
+            emsgf.write(str(emsg))
+            emsgf.flush()
+
+            with gpg_import(os.path.realpath(sf)) as kf:
+                assert gpg_print(emsgf.name) == dmsg.message
+
+    @pytest.mark.run(after='test_encrypt_message_throw_keyid')
+    @pytest.mark.parametrize('sf,cipher',
+                             itertools.product(sorted(glob.glob('tests/testdata/keys/*.sec.asc')), sorted(SymmetricKeyAlgorithm)))
+    def test_decrypt_message_thrown_keyid(self, sf, cipher, gpg_import, gpg_print):
+        sec, _ = PGPKey.from_file(sf)
+        if (sec.fingerprint, cipher, True) not in self.msgs:
+            pytest.skip('Message not present; see test_encrypt_message_throw_keyid skip or xfail reason')
+
+        emsg = self.msgs[(sec.fingerprint, cipher, True)]
         dmsg = sec.decrypt(emsg)
 
         assert dmsg.message == "This message will have been encrypted"
