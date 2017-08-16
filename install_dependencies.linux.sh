6769b621bd2c12f9cae577a76a4c2eab19baba74
Michael Greene <a.minor.threat@gmail.com>
2017-08-15 22:40:43 -0700

    test^3

--- install_dependencies.linux.sh
+++ install_dependencies.linux.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# do nothing because we are using container-based travis-ci now
-# sudo apt-get update
-# sudo apt-get install -y libffi-dev gnupg2 pgpdump libgpgme11-dev
+sudo apt-get update
+sudo apt-get install -y libffi-dev gnupg2 pgpdump libgpgme11-dev
