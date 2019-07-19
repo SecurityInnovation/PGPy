d16178fb0104d6699cdc3a89ee3fd8c1b87a841a
rot42 <rot42@protonmail.com>
2019-07-19 07:22:47 +0200

    fixed failing tests
    Commit e163a0cf22735d639ecaaeab4cd6c903acb9a5c5 made creation and
    expiration date relative to current time which does not match content in
    revochiio.asc. Reverted commit and asserted the key as expired.
    
    test_10_exceptions.py: test_encrypt_sessionkey_wrongtype did not have a
    wrong sessionkey type and therefore was not raising TypeError.
    
    pytest.raises() message parameter is deprecated
    https://github.com/pytest-dev/pytest/pull/4539
    
    EllipticCurveOID does not have a name attribute, the name of the curve
    is stored in EllipticCurveOID.curve.name

--- tests/test_10_exceptions.py
+++ tests/test_10_exceptions.py
@@ -380,7 +380,7 @@ class TestPGPMessage(object):
     def test_encrypt_sessionkey_wrongtype(self):
         msg = PGPMessage.new('asdf')
         with pytest.raises(TypeError):
-            msg.encrypt('asdf', sessionkey=bytearray(b'asdf1234asdf1234'), cipher=SymmetricKeyAlgorithm.AES128)
+            msg.encrypt('asdf', sessionkey=1234567890, cipher=SymmetricKeyAlgorithm.AES128)
 
     def test_parse_wrong_magic(self):
         msgtext = _read('tests/testdata/messages/message.signed.asc').replace('MESSAGE', 'EMSSAGE')
