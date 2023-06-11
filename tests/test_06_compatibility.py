a3941e07580e1d350b882a69e767617a759d3bda
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2023-06-10 23:37:58 -0400

    Tests: Check on forward-compatibility failures
    These tests are pulled from the OpenPGP Interoperability Test Suite,
    in particular from those tests tagged "forward-compat":
    
       https://tests.sequoia-pgp.org/?q=forward-compat
    
    These failures make it difficult to use PGPy in an OpenPGP ecosystem
    that can evolve, because PGPy will complain about OpenPGP objects that
    contain something it doesn't understand, even though the rest of the
    message is otherwise comprehensible and usable.
    
    See Justus Winter's message to openpgp@ietf.org describing his
    concerns:
    
    https://mailarchive.ietf.org/arch/msg/openpgp/QUiEKx3PQeJOXnkcvvnuHpv739M
    
    I've selected specific examples that PGPy is known to currently fail with.

---
+++ tests/test_06_compatibility.py
@@ -0,0 +1,58 @@
+# coding=utf-8
+""" ensure that we don't crash on surprising messages
+"""
+import pytest
+
+from pgpy import PGPKey, PGPMessage
+from pgpy.constants import SecurityIssues
+import glob
+
+class TestPGP_Compatibility(object):
+    # test compatibility:
+    # - Armored object with (non-ASCII) UTF-8 comments
+    # - signatures with unknown pubkey algorithms
+    # - certs with certifications from unknown algorithms
+    # - certs with subkeys with unknown algorithms
+    # - certs with ECC subkeys with unknown curves
+    # - encrypted messages with surprising parts
+    def test_import_unicode_armored_cert(self) -> None:
+        k:PGPKey
+        (k, _) = PGPKey.from_file('tests/testdata/compatibility/ricarda.pgp')
+        pytest.xfail('Cannot handle UTF-8 (non-ASCII) comments in armored certificate')
+        assert k.check_soundness() == SecurityIssues.OK
+
+    @pytest.mark.parametrize('sig', glob.glob('*.sig', root_dir='tests/testdata/compatibility'))
+    def test_bob_sig_from_multisig(self, sig:str)-> None:
+        k:PGPKey
+        (k, _) = PGPKey.from_file('tests/testdata/compatibility/bob.pgp')
+        msg = 'Hello World :)'
+        pytest.xfail(f'Cannot handle detached signature objects with more than one signature present (see https://github.com/SecurityInnovation/PGPy/issues/197)')
+
+    def test_cert_unknown_algo(self) -> None:
+        k:PGPKey
+        pytest.xfail('cannot handle certificates containing certifications made using unknown pubkey algorithms')
+        (k, _) = PGPKey.from_file('tests/testdata/compatibility/bob_with_unknown_alg_certification.pgp')
+        assert k.check_soundness() == SecurityIssues.OK
+
+    def test_cert_unknown_subkey_algo(self) -> None:
+        k:PGPKey
+        pytest.xfail('cannot handle certificates containing subkeys with unknown pubkey algorithms')
+        (k, _) = PGPKey.from_file('tests/testdata/compatibility/bob_with_unknown_subkey_algorithm.pgp')
+        assert k.check_soundness() == SecurityIssues.OK
+
+    @pytest.mark.parametrize('flavor', ['ecdsa', 'eddsa', 'ecdh'])
+    def test_cert_unknown_curve(self, flavor:str) -> None:
+        k:PGPKey
+        pytest.xfail(f'cannot handle certificates containing subkeys with unknown OIDs for {flavor}')
+        (k, _) = PGPKey.from_file(f'tests/testdata/compatibility/bob_with_unknown_{flavor}_curve.pgp')
+        assert k.check_soundness() == SecurityIssues.OK
+
+    @pytest.mark.parametrize('msg', glob.glob('*.msg', root_dir='tests/testdata/compatibility'))
+    def test_unknown_message(self, msg:str)-> None:
+        k:PGPKey
+        (k, _) = PGPKey.from_file('tests/testdata/compatibility/bob-key.pgp')
+        pytest.xfail('cannot handle unknowns in message formats')
+        msg:PGPMessage = PGPMessage.from_file(f'tests/testdata/compatibility/{msg}')
+        cleartext:PGPMessage = k.decrypt(msg)
+        assert not cleartext.is_encrypted
+        assert cleartext.message.startswith(b'Encrypted')
